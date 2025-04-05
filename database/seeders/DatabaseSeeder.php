9897a70d2980a06479b9ca184d989440ff260e3c
junevig <67835792+junevig@users.noreply.github.com>
2025-04-05 16:30:34 -0500

    Seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Storage::deleteDirectory('products');
+        Storage::makeDirectory('products');
+
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Julio Villacorta',
+            'email' => 'dev@coder.com',
+            'password' => bcrypt('12345678')
         ]);
+
+        $this->call([
+            FamilySeeder::class,
+            OptionSeeder::class
+        ]);
+
+        Product::factory(150)->create();
     }
 }
